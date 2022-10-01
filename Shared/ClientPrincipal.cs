21ad79316847420c1b39880a9afe09f364cada79
Damiano Andresini <d.andresini@cotrap.it>
2022-10-01 07:13:46 +0200

    fix change in function

--- Shared/ClientPrincipal.cs
+++ Shared/ClientPrincipal.cs
@@ -5,11 +5,11 @@ namespace BlazorApp.Shared
 {
     public class ClientPrincipal
     {
-        public string IdentityProvider { get; set; }
-        public string UserId { get; set; }
-        public string UserDetails { get; set; }
-        public IEnumerable<string> UserRoles { get; set; }
-        public IEnumerable<CustomClaim> Claims { get; set; }
+        public string? IdentityProvider { get; set; }
+        public string? UserId { get; set; }
+        public string? UserDetails { get; set; }
+        public IEnumerable<string>? UserRoles { get; set; }
+        public IEnumerable<CustomClaim>? Claims { get; set; }
     }
 
     public class CustomClaim
